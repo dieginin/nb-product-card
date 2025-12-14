16901bdd04ce4efc1f6c3236b1056ffd5ac5ede6
dieginin <diegobalestra9@gmail.com>
2025-12-14 09:28:29 -0800

    Remove obsolete test file for ReactDOM rendering

--- test/blah.test.tsx
+++
@@ -1,9 +0,0 @@
-import * as ReactDOM from 'react-dom/client';
-
-describe('it', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    const root = ReactDOM.createRoot(div);
-    root.unmount();
-  });
-});
